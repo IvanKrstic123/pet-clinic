d7fcb7d58ef0e175b915f97e38d1b8e6b1c2ed5d
Ivan Krstic <krstic8687@yahoo.com>
2021-06-06 16:04:13 +0200

    added visits, update owner with pet relationship. closes #2

--- pet-clinic/pet-clinic-data/src/main/java/model/Pet.java
+++ pet-clinic/pet-clinic-data/src/main/java/model/Pet.java
@@ -5,8 +5,8 @@ import java.time.LocalDate;
 public class Pet extends BaseEntity{
 
     private PetType petType;
-    private Owner owner;
     private LocalDate birthDate;
+    private Owner owner;
 
     public PetType getPetType() {
         return petType;
@@ -16,19 +16,19 @@ public class Pet extends BaseEntity{
         this.petType = petType;
     }
 
-    public Owner getOwner() {
-        return owner;
-    }
-
-    public void setOwner(Owner owner) {
-        this.owner = owner;
-    }
-
     public LocalDate getBirthDate() {
         return birthDate;
     }
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
     }
 }
