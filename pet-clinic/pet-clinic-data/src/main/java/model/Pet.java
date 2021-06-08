ad32ec4311c1f869bcbafbd71bcd0bd74f83c79d
Ivan Krstic <krstic8687@yahoo.com>
2021-06-08 17:17:48 +0200

    Created Visit Entity. closes #14

--- pet-clinic/pet-clinic-data/src/main/java/model/Pet.java
+++ pet-clinic/pet-clinic-data/src/main/java/model/Pet.java
@@ -2,6 +2,8 @@ package model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -20,6 +22,9 @@ public class Pet extends BaseEntity{
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
@@ -54,4 +59,11 @@ public class Pet extends BaseEntity{
     }
 
 
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
+    }
 }
