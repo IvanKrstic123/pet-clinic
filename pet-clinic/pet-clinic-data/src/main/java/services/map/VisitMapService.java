7a869e5da43d2aec93ad46abf1d2bf224905b0eb
Ivan Krstic <krstic8687@yahoo.com>
2021-06-09 11:29:48 +0200

    Added Map Based Visit Service. closes #23

---
+++ pet-clinic/pet-clinic-data/src/main/java/services/map/VisitMapService.java
@@ -0,0 +1,35 @@
+package services.map;
+
+import model.Visit;
+import repositories.VisitRepository;
+import services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return (Set<Visit>) super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
