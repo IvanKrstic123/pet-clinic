55d8a63f0e5c20a188ee971b595a4c181424fdb2
Ivan Krstic <krstic8687@yahoo.com>
2021-06-09 11:11:08 +0200

    Spring Data Jpa for Vet Service. closes #22

---
+++ pet-clinic/pet-clinic-data/src/main/java/services/springdatajpa/VisitSDJpaService.java
@@ -0,0 +1,45 @@
+package services.springdatajpa;
+
+import model.Visit;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import repositories.VisitRepository;
+import services.VisitService;
+
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VisitSDJpaService implements VisitService {
+
+    private final VisitRepository visitRepository;
+
+    public VisitSDJpaService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        return (Set<Visit>) visitRepository.findAll();
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return visitRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return visitRepository.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        visitRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        visitRepository.deleteById(aLong);
+    }
+}
