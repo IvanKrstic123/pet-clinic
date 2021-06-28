d4f02c822db5831047194fa6ef6878e82108fd4a
Ivan Krstic <krstic8687@yahoo.com>
2021-06-29 01:21:12 +0200

    Fixing Syntax Errors

--- pet-clinic/pet-clinic-data/src/main/java/repositories/OwnerRepository.java
+++ pet-clinic/pet-clinic-data/src/main/java/repositories/OwnerRepository.java
@@ -2,6 +2,9 @@ package repositories;
 
 import model.Owner;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+    Owner findByLastName(String lastName);
 }
