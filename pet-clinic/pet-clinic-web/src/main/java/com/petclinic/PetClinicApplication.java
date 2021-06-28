d3671dd2ee4c040dfcd3277d47517db6c1f73bd9
Ivan Krstic <krstic8687@yahoo.com>
2021-06-29 01:26:42 +0200

    Fixing Bug. Component Scan not picking up Repositories. closes #36

--- pet-clinic/pet-clinic-web/src/main/java/com/petclinic/PetClinicApplication.java
+++ pet-clinic/pet-clinic-web/src/main/java/com/petclinic/PetClinicApplication.java
@@ -4,15 +4,18 @@ import com.petclinic.bootstrap.DataLoader;
 import com.petclinic.controllers.OwnerController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import services.map.OwnerServiceMap;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import services.springdatajpa.OwnerSDJpaService;
 
 @SpringBootApplication
-@ComponentScan(basePackageClasses = {OwnerController.class, OwnerServiceMap.class, DataLoader.class})
+@EnableJpaRepositories("repositories")
+@EntityScan("model")
+@ComponentScan(basePackageClasses = {OwnerController.class, OwnerSDJpaService.class, DataLoader.class })
 public class PetClinicApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetClinicApplication.class, args);
 	}
-
 }
