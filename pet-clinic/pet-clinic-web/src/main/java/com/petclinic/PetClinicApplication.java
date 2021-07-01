908f165658118c8904aa325b15e577875fc1a28f
Ivan Krstic <krstic8687@yahoo.com>
2021-07-01 16:12:42 +0200

    Refactoring code with Project Lombok. closes #28

--- pet-clinic/pet-clinic-web/src/main/java/com/petclinic/PetClinicApplication.java
+++ pet-clinic/pet-clinic-web/src/main/java/com/petclinic/PetClinicApplication.java
@@ -1,6 +1,7 @@
 package com.petclinic;
 
 import com.petclinic.bootstrap.DataLoader;
+import com.petclinic.controllers.OwnerController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
